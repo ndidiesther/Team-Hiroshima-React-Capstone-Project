ee8529f6aff1a1efcfec0d1bdc8ce1407c6bdfba
ndidiesther <estherchibogu@gmail.com>
2021-11-07 17:50:18 +0100

    worked on the interface

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home'; 
+import Home from './Home'; 
+import {BrowserRouter as Router, Switch, Route } from 
+"react-router-dom";
 
 function App() { 
-  return (
-    <div className="App">
+return (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/search">
+          <h1>This is the search Page</h1>
+        </Route>
+        <Route path="/">
+        <Home />
+        </Route>
+      </Switch>
+        
+    </Router>
 
-      <Home />
 
-    </div>
-  );
+  </div>
+);
 }
 
 export default App;
