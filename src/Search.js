d4989ca5e645c3dbc92d95f06371f2c2554ffdff
ndidiesther <estherchibogu@gmail.com>
2021-11-07 18:51:46 +0100

    worked on the interface

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 
 const Search = () => {
+    const [input, setInput] = useState('')
+
+    const search = e => {
+        e.preventDefault();
+        console.log('You hit the enter button', input)
+    }
     return (
-        <div className="search"> 
+        <form className="search"> 
             <div className="search_input">
                 <SearchIcon className="search_inputIcon" />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value) } />
                 <MicIcon />
             </div>
             <div className="search_buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
